b745736da39799317ff1983954ade4f79cc6fa25
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-01-19 15:45:17 -0300

    Repositorio atualizada

--- aula_02/main.py
+++ aula_02/main.py
@@ -2,25 +2,25 @@ import cv2
 
 # Para ler uma imagem é usada o método cv2.imread("imagem.jpg", 0)
 # o segundo argumento "0" define a imagem na escala de cinza
-img_cinza = cv2.imread("imagem.jpg", 0)
+img = cv2.imread("imagem.jpg", 0)
 
 escala = 20   # porcentagem %
 
 # Largura e altura da imagem
-height, width = img_cinza.shape
+altura, largura = img.shape
 
 # Redimencionando a imagem
-n_width = int(width * (escala / 100))
-n_height = int(height * (escala / 100))
+n_largura = int(largura * (escala / 100))
+n_altura = int(altura * (escala / 100))
 
 # Tupla contendo as novas dimensões da imagem
-dim = (n_width, n_height)
+dim = (n_largura, n_altura)
 
 # Redimensionando a imagem usando o método cv2.resize()
-img_redimencionada = cv2.resize(img_cinza, dim, cv2.INTER_AREA)
+img_redimencionada = cv2.resize(img, dim, cv2.INTER_AREA)
 
 # Para visualizar a imagem em uma tela, usa-se o método cv2.imshow()
-cv2.imshow("Imagem Original", img_cinza)
+cv2.imshow("Imagem Original", img)
 
 # Mostrando imagem redimencionada
 cv2.imshow("Imagem Redimencionanda", img_redimencionada)
@@ -33,4 +33,4 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 # O método cv2.imwrite() é usada para salvar uma imagem.
-cv2.imwrite("Imagem na Escala de Cinza.png", img_cinza)
+cv2.imwrite("Imagem na Escala de Cinza.png", img)
---
+++ aula_03/main.py
@@ -0,0 +1,24 @@
+import cv2
+
+img = cv2.imread("imagem.jpg", 1)
+
+escala = 20  # Escala de redução em porcentagem %
+
+altura, largura, _ = img.shape
+
+n_largura = int(largura * escala/100)
+n_altura = int(altura * escala/100)
+
+dim = (n_largura, n_altura)
+
+n_img = cv2.resize(img, dim, cv2.INTER_AREA)
+
+cv2.imshow("Imagem Original", img)
+
+cv2.imshow("Imagem Modificada", n_img)
+
+cv2.waitKey(0)
+
+cv2.imwrite("imagem_modificada.png", n_img)
+
+cv2.destroyAllWindows()
